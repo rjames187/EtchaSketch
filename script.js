ebb206862a4d1bd0ddf89183b2e0bfa14b1586e3
Rory <rory.james2021@gmail.com>
2022-07-17 17:48:12 -0500

    add imperative grid construction and unit border

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ let numDivs = 16;
 // side length of a grid div;
 let sl = GRID_CONTAINER_SIDE_LENGTH / numDivs;
 
+// construct grid
+let container = document.getElementById("container");
+
+for (let i = 0; i < numDivs; i++) {
+    // construct row with attributes
+    const row = document.createElement("div");
+    row.classList.add("row");
+    row.style.cssText = `height: ${sl}px; width: ${GRID_CONTAINER_SIDE_LENGTH}px`;
+    
+    for (let j = 0; j < numDivs; j++) {
+        // construct unit with attributes
+        const unit = document.createElement("div");
+        unit.classList.add("unit");
+        unit.style.cssText = `height: ${sl}px; width: ${sl}px`;
+        row.appendChild(unit);
+    }
+    
+    container.appendChild(row);
+}
+
