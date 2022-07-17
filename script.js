d0a676f9a261a525d454d6177aa572c1f04a057f
Rory <rory.james2021@gmail.com>
2022-07-17 17:30:45 -0500

    add calculation for length of a grid unit

---
+++ script.js
@@ -0,0 +1,9 @@
+// side length of the grid container in pixels
+const GRID_CONTAINER_SIDE_LENGTH = 800;
+
+// number of square divs in a row in the grid container
+let numDivs = 16;
+
+// side length of a grid div;
+let sl = GRID_CONTAINER_SIDE_LENGTH / numDivs;
+
